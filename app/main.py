13bf054bebee2cb9b80aaf2187a11e54d694dbad
tenifayo <tennisco19@gmail.com>
2026-05-20 16:01:34 +0100

    Add PostgreSQL + SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,42 +1,53 @@
-from fastapi import FastAPI, HTTPException
-from app.schemas import TaskCreate, TaskUpdate
-from app.service import TaskService
-
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from app.database import Base, engine
+from app import models
+from app.deps import get_db
 
 app = FastAPI()
-service = TaskService()
 
 
-@app.get("/")
-def root():
-    return {"message": "Task API running"}
+Base.metadata.create_all(bind=engine)
 
 
 @app.post("/tasks")
-def create_task(task: TaskCreate):
-    return service.create_task(
-        task.title,
-        task.description,
-        task.priority
+def create_task(title: str, description: str = None, priority: int = 1, db: Session = Depends(get_db)):
+    task = models.Task(
+        title=title,
+        description=description,
+        priority=priority
     )
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
 
 
 @app.get("/tasks")
-def list_tasks():
-    return service.get_tasks()
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
 
 
 @app.patch("/tasks/{task_id}")
-def update_task(task_id: int, task: TaskUpdate):
-    updated = service.update_task(task_id, task.model_dump())
-    if not updated:
+def update_task(task_id: int, completed: bool, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
+    if not task:
         raise HTTPException(status_code=404, detail="Task not found")
-    return updated
+
+    task.completed = completed
+    db.commit()
+    return task
 
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    deleted = service.delete_task(task_id)
-    if not deleted:
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
+    if not task:
         raise HTTPException(status_code=404, detail="Task not found")
-    return {"message": "Deleted"}
+
+    db.delete(task)
+    db.commit()
+
+    return {"message": "deleted"}
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
+from sqlalchemy import Column, Integer, String, Boolean
+from app.database import Base
 
 
 @dataclass
@@ -9,3 +11,13 @@ class Task:
     description: Optional[str]
     completed: bool = False
     priority: int = 1  # 1 = low, 2 = medium, 3 = high
+
+
+class TaskModel(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+    priority = Column(Integer, default=1)
